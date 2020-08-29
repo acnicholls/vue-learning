79148d7243cbf2dfb7143227ee61d0da5e853ad4
anthony <ac.nicholls@gmail.com>
2020-08-29 17:22:02 -0400

    hot reloading fix, main page started

---
+++ build-a-bot/vue.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    configureWebpack: {
+        devServer:{
+            clientLogLevel: 'info',
+            watchOptions:
+            {
+                poll: true
+            }
+        }
+    }
+}
