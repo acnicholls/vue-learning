ea8e0775f52d1a497070bf0f587ebf7ef28bed22
anthony <ac.nicholls@gmail.com>
2020-08-30 18:47:04 -0400

    parts browsing and sidebars

--- build-a-bot/src/build/PartSelector.vue
+++ build-a-bot/src/build/PartSelector.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="part" :class="position">
-    <img :src="selectedPart.src" title="arm"/>
+    <img :src="selectedPart.src" @click="showPartInfo()" title="arm"/>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span class="sale" v-show="selectedPart.onSale">Sale!</span>
@@ -48,6 +48,15 @@ export default {
     this.emitSelectedPart();
   },
   methods: {
+    showPartInfo() {
+      this.$router.push({
+        name: 'Parts',
+        params: {
+          partType: this.selectedPart.partType,
+          id: this.selectedPart.id,
+        },
+      });
+    },
     emitSelectedPart() {
       this.$emit('partSelected', this.selectedPart);
     },
@@ -93,6 +102,7 @@ export default {
 }
 .part img {
   width:165px;
+  cursor: pointer;
 }
 .top {
   border-bottom: none;
