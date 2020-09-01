7f1ed9ac73e2cc7c67d5a4fccf566c26ab6dcd9b
anthony <ac.nicholls@gmail.com>
2020-08-31 22:35:57 -0400

    now using API for part info

--- build-a-bot/vue.config.js
+++ build-a-bot/vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   configureWebpack: {
     devServer: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8082',
+          changeOrigin: true,
+        },
+      },
       clientLogLevel: 'info',
       watchOptions:
       {
